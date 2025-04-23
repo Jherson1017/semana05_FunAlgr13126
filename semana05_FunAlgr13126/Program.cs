79b75ef29247dd825a0a7b6e1016f2a977719714
Jherson Alaya <jherson@upn.pe>
2025-04-23 17:04:52 -0500

    Agregar archivos de proyecto.

---
+++ semana05_FunAlgr13126/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace semana05_FunAlgr13126
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
