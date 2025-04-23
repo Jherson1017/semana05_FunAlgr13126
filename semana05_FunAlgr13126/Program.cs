68e84c71d7e91000e2f31d432eb7ab980effafad
Jherson Alaya <jherson@upn.pe>
2025-04-23 17:32:52 -0500

    Ejercicio1

--- semana05_FunAlgr13126/Program.cs
+++ semana05_FunAlgr13126/Program.cs
@@ -10,6 +10,27 @@ namespace semana05_FunAlgr13126
     {
         static void Main(string[] args)
         {
+            int num;
+            int suma;
+            Console.Write("Ingrese numero: ");
+            num = int.Parse(Console.ReadLine());
+            suma = 0;
+            for (int x = 1; x < num; x++)
+            {
+                if (num % x == 0)
+                {
+                    suma = suma + x;
+                }
+            }
+            if(num == suma)
+            {
+                Console.WriteLine("Si es perfecto");
+            }
+            else
+            {
+                Console.WriteLine("No es perfecto");
+            }
+            Console.ReadKey();
         }
     }
 }
